5384095eb573df7b06f6802d7b98246562ec7a10
Adail Silva <adail101@hotmail.com>
2026-06-03 12:46:23 -0300

    Acerto/Melhorias frontend.

--- 05.chirpstack_consumer/app_chirpstack_consumer_frontend/src/app/layout/components/icons/customers/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './britemank';
-export * from './limerantz';
-export * from './mistranet';
-export * from './streamlinz';
-export * from './trimzales';
-export * from './wavelength';
-export * from './zentrailms';
